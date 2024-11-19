b5a4e6281dbf8cede91b6999aab64c6aa6a039d4
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-11-19 00:20:28 -0800

    Compiler errors

--- astoria-game/Assets/Construction/Scripts/ConstructionCore.cs
+++ astoria-game/Assets/Construction/Scripts/ConstructionCore.cs
@@ -531,7 +531,7 @@ public class ConstructionCore : InputHandlerBase, IStartExecution
     {
       foreach (ConstructionObjectCost cost in data.Cost)
       {
-        if (!ResourceHolder.Instance.InventoryUI.GetItemCount(cost.Item, cost.Amount))
+        if (!ResourceHolder.Instance.InventoryUI.ItemCountInInventory(cost.Item, cost.Amount))
         {
           errorText = "You do not have the required resources";
           return false;
