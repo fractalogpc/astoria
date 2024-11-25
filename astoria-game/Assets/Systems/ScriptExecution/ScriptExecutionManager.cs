121776ce8d48508818739bd245351d2771211e60
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-11-24 17:31:21 -0800

    Allow instantiating objects with custom script execution

--- astoria-game/Assets/Systems/ScriptExecution/ScriptExecutionManager.cs
+++ astoria-game/Assets/Systems/ScriptExecution/ScriptExecutionManager.cs
@@ -1,104 +1,104 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Interfaces for classes that require ordered execution of OnEnable.
-/// </summary>
 public interface IOnEnableExecution
 {
-  void InitializeOnEnable();
+    void InitializeOnEnable();
 }
 
-/// <summary>
-/// Interfaces for classes that require ordered execution of Start.
-/// </summary>
 public interface IStartExecution
 {
-  void InitializeStart();
+    void InitializeStart();
 }
 
-
-/// <summary>
-/// Central manager that handles the ordered execution of OnEnable and Start methods for all registered scripts.
-/// Ensures that all OnEnable methods run first, followed by all Start methods in a controlled order.
-/// </summary>
 public class ScriptExecutionManager : PersistentSingleton<ScriptExecutionManager>
 {
-  // Lists to store scripts that need ordered execution of OnEnable and Start
-  private readonly List<IOnEnableExecution> _onEnableScripts = new List<IOnEnableExecution>();
-  private readonly List<IStartExecution> _startScripts = new List<IStartExecution>();
+    private readonly List<IOnEnableExecution> _onEnableScripts = new List<IOnEnableExecution>();
+    private readonly List<IStartExecution> _startScripts = new List<IStartExecution>();
+    private readonly HashSet<object> _initializedOnEnableScripts = new HashSet<object>();
+    private readonly HashSet<object> _initializedStartScripts = new HashSet<object>();
 
-  private bool _isOnEnableExecuted = false;
+    private bool _isInitialized = false;
 
-  /// <summary>
-  /// Registers a script for ordered execution.
-  /// </summary>
-  /// <param name="script">Script implementing IOrderedExecution to register.</param>
-  public void RegisterScript(IOnEnableExecution script)
-  {
-    if (!_onEnableScripts.Contains(script))
+    /// <summary>
+    /// Registers a script for ordered execution and immediately initializes it if the manager has already run its initial execution.
+    /// </summary>
+    public void RegisterScript(IOnEnableExecution script)
     {
-      _onEnableScripts.Add(script);
-    }
-  }
-
-  /// <summary>
-  /// Registers a script for ordered execution.
-  /// </summary>
-  /// <param name="script">Script implementing IOrderedExecution to register.</param>
-  public void RegisterScript(IStartExecution script)
-  {
-    if (!_startScripts.Contains(script)) _startScripts.Add(script);
-  }
-
-  /// <summary>
-  /// Initializes all registered scripts by calling their InitializeOnEnable methods.
-  /// </summary>
-  public void ExecuteOnEnable()
-  {
-    foreach (var script in _onEnableScripts)
-    {
-      script.InitializeOnEnable();
-    }
-    _isOnEnableExecuted = true;
-  }
-
-  /// <summary>
-  /// Initializes all registered scripts by calling their InitializeStart methods.
-  /// This should only be called after ExecuteOnEnable has run.
-  /// </summary>
-  public void ExecuteStart()
-  {
-    if (!_isOnEnableExecuted)
-    {
-      Debug.LogError("ExecuteOnEnable must be called before ExecuteStart.");
-      return;
+        if (!_onEnableScripts.Contains(script))
+        {
+            _onEnableScripts.Add(script);
+            // If we've already done our initial execution, immediately initialize this new script
+            if (_isInitialized && !_initializedOnEnableScripts.Contains(script))
+            {
+                script.InitializeOnEnable();
+                _initializedOnEnableScripts.Add(script);
+            }
+        }
     }
 
-    foreach (var script in _startScripts)
+    /// <summary>
+    /// Registers a script for ordered execution and immediately initializes it if the manager has already run its initial execution.
+    /// </summary>
+    public void RegisterScript(IStartExecution script)
     {
-      script.InitializeStart();
+        if (!_startScripts.Contains(script))
+        {
+            _startScripts.Add(script);
+            // If we've already done our initial execution, immediately initialize this new script
+            if (_isInitialized && !_initializedStartScripts.Contains(script))
+            {
+                script.InitializeStart();
+                _initializedStartScripts.Add(script);
+            }
+        }
     }
-  }
 
-  private void Start()
-  {
-    ExecuteOnEnable();
-    Debug.Log("<color=green>ScriptExecutionManager</color> OnEnable");
+    public void ExecuteOnEnable()
+    {
+        foreach (var script in _onEnableScripts)
+        {
+            if (!_initializedOnEnableScripts.Contains(script))
+            {
+                script.InitializeOnEnable();
+                _initializedOnEnableScripts.Add(script);
+            }
+        }
+    }
 
-    ExecuteStart();
-    Debug.Log("<color=green>ScriptExecutionManager</color> Start");
-  }
+    public void ExecuteStart()
+    {
+        foreach (var script in _startScripts)
+        {
+            if (!_initializedStartScripts.Contains(script))
+            {
+                script.InitializeStart();
+                _initializedStartScripts.Add(script);
+            }
+        }
+    }
 
-  protected override void Awake()
-  {
-    base.Awake();
-  }
+    private void Start()
+    {
+        ExecuteOnEnable();
+        Debug.Log("<color=green>ScriptExecutionManager</color> OnEnable");
 
-  private void OnDestroy()
-  {
-    // Clear lists to prevent memory leaks
-    _onEnableScripts.Clear();
-    _startScripts.Clear();
-  }
+        ExecuteStart();
+        Debug.Log("<color=green>ScriptExecutionManager</color> Start");
+        
+        _isInitialized = true;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void OnDestroy()
+    {
+        _onEnableScripts.Clear();
+        _startScripts.Clear();
+        _initializedOnEnableScripts.Clear();
+        _initializedStartScripts.Clear();
+    }
 }
