0756f1cdd30661986a978e306da3d49b517fc3a0
Elliot <ejdalessa@gmail.com>
2024-11-28 20:40:06 -0800

    Text faces player

--- astoria-game/Assets/Utilities/LookAtTarget.cs
+++ astoria-game/Assets/Utilities/LookAtTarget.cs
@@ -1,19 +1,44 @@
+using Mirror.BouncyCastle.Asn1.X509;
 using UnityEngine;
 
-public class LookAtTarget : MonoBehaviour
+public class LookAtTarget : MonoBehaviour, IStartExecution
 {
-  [SerializeField] private Transform target;
   [SerializeField] private bool IgnoreXAxis;
+
+  private Transform target;
+
+  private bool _tryInitialize;
+  private bool _initialized;
+
+  public void InitializeStart() {
+    ResourceHolder.Instance.GameStateHandler.AddOnStateEnter(GameStateHandler.GameState.Playing, () => {
+      Initialize();
+    });
+  }
   
+  private void Initialize() {
+    _tryInitialize = true;
+    GameObject targetObject = LocalPlayerReference.Instance.LocalPlayer;
+
+    if (targetObject != null) {
+      target = targetObject.transform;
+      _initialized = true;
+    }
+  }
+
   private void Update()
   {
-    if (target)
+    if (_initialized)
     {
       if (IgnoreXAxis)
       {
         transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
       } else {
         transform.LookAt(target);
+      }
+    } else {
+      if (_tryInitialize) {
+        Initialize();
       }
     }
   }
