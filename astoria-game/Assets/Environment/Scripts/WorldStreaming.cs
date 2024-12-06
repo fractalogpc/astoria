59266325f79e78a49fe2b51f74126a9ebe32280b
DoomKitty87 <doomkitty887@gmail.com>
2024-12-06 11:39:16 -0800

    work on terrain streaming

---
+++ astoria-game/Assets/Environment/Scripts/WorldStreaming.cs
@@ -0,0 +1,182 @@
+using UnityEngine;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+public class EditorUpdate
+{
+
+  static EditorUpdate() {
+      EditorApplication.update += Update;
+  }
+
+  static void Update() {
+    if (!Application.isPlaying) {
+      foreach (var worldStreaming in GameObject.FindObjectsOfType<WorldStreaming>()) {
+        worldStreaming.EditorUpdate();
+      }
+    }
+  }
+
+}
+#endif
+
+public class WorldStreaming : MonoBehaviour
+{
+  [SerializeField] private int gridWidth = 2;
+  [SerializeField] private int gridHeight = 2;
+  [SerializeField] private float tileWidth = 1000f;
+  [SerializeField] private float tileHeight = 1000f;
+  [SerializeField] private bool initializeTileWithSize = true;
+  [SerializeField] private Material terrainMaterial;
+  [SerializeField] private bool initializeTileWithMaterial = true;
+  [SerializeField] private int defaultTerrainResolution = 1025;
+  [SerializeField] private Transform player;
+  [SerializeField] private Transform terrainParent;
+  [SerializeField] private float loadDistance = 100f;
+  [SerializeField] private float unloadDistance = 150f;
+
+  private Dictionary<Vector2Int, Terrain> loadedTerrains = new Dictionary<Vector2Int, Terrain>();
+
+  public void EditorUpdate() {
+    LoadClickedSquares();
+  }
+
+  private void Update() {
+    if (Application.isPlaying) {
+      HandlePlayMode();
+    }
+  }
+
+  private void HandlePlayMode() {
+    Vector2Int playerGridPos = new Vector2Int(
+      Mathf.FloorToInt(player.position.x / gridWidth),
+      Mathf.FloorToInt(player.position.z / gridHeight)
+    );
+
+    List<Vector2Int> tilesToLoad = new List<Vector2Int>();
+    List<Vector2Int> tilesToUnload = new List<Vector2Int>();
+
+    foreach (var tile in loadedTerrains.Keys) {
+      float distance = Vector3.Distance(player.position, new Vector3(tile.x * gridWidth, 0, tile.y * gridHeight));
+      if (distance > unloadDistance) {
+        tilesToUnload.Add(tile);
+      }
+    }
+
+    for (int x = -1; x <= 1; x++) {
+      for (int y = -1; y <= 1; y++) {
+        Vector2Int tilePos = new Vector2Int(playerGridPos.x + x, playerGridPos.y + y);
+        if (!loadedTerrains.ContainsKey(tilePos)) {
+          float distance = Vector3.Distance(player.position, new Vector3(tilePos.x * gridWidth, 0, tilePos.y * gridHeight));
+          if (distance <= loadDistance) {
+            tilesToLoad.Add(tilePos);
+          }
+        }
+      }
+    }
+
+    foreach (var tile in tilesToUnload) {
+      UnloadTerrain(tile);
+    }
+
+    foreach (var tile in tilesToLoad) {
+      LoadTerrain(tile);
+    }
+  }
+
+  private void LoadTerrain(Vector2Int tilePos) {
+    // Implement your terrain loading logic here
+    // For example, load terrain data from disk and instantiate it
+    string terrainPath = $"Terrains/Terrain_{tilePos.x}_{tilePos.y}";
+    TerrainData terrainData = Resources.Load<TerrainData>(terrainPath);
+      GameObject terrainPrefab = Resources.Load<GameObject>("TerrainPrefab");
+      GameObject terrainGO;
+      if (terrainPrefab != null) {
+        terrainGO = Instantiate(terrainPrefab, Vector3.zero, Quaternion.identity);
+        terrainGO.transform.SetParent(terrainParent);
+      } else {
+        Debug.LogWarning("Terrain prefab not found in resources.");
+        return;
+      }
+    if (terrainData != null) {
+      // Set the terrain data
+      terrainGO.GetComponent<Terrain>().terrainData = terrainData;
+      terrainGO.GetComponent<TerrainCollider>().terrainData = terrainData;
+      terrainGO.transform.position = new Vector3(tilePos.x * tileWidth, 0, tilePos.y * tileWidth);
+      loadedTerrains[tilePos] = terrainGO.GetComponent<Terrain>();
+    } else {
+      Debug.LogWarning($"Terrain data not found at path: {terrainPath}");
+      // Create a new terrain
+      terrainGO.GetComponent<Terrain>().terrainData = new TerrainData();
+      terrainGO.GetComponent<TerrainCollider>().terrainData = terrainGO.GetComponent<Terrain>().terrainData;
+      terrainGO.transform.position = new Vector3(tilePos.x * tileWidth, 0, tilePos.y * tileWidth);
+      loadedTerrains[tilePos] = terrainGO.GetComponent<Terrain>();
+
+      // Set terrain resolution
+      terrainGO.GetComponent<Terrain>().terrainData.heightmapResolution = defaultTerrainResolution;
+      terrainGO.GetComponent<Terrain>().terrainData.alphamapResolution = defaultTerrainResolution;
+      terrainGO.GetComponent<Terrain>().terrainData.baseMapResolution = defaultTerrainResolution;
+
+      // Set terrain default values
+      if (initializeTileWithSize) {
+        terrainGO.GetComponent<Terrain>().terrainData.size = new Vector3(tileWidth, tileHeight, tileWidth);
+      }
+
+      if (initializeTileWithMaterial) {
+        terrainGO.GetComponent<Terrain>().materialType = Terrain.MaterialType.Custom;
+        terrainGO.GetComponent<Terrain>().materialTemplate = terrainMaterial;
+      }
+
+      if ()
+
+      // Save the terrain data to disk
+      #if UNITY_EDITOR
+            AssetDatabase.CreateAsset(terrainGO.GetComponent<Terrain>().terrainData, "Assets/Resources/" + terrainPath + ".asset");
+            AssetDatabase.SaveAssets();
+      #else
+            Debug.LogWarning("Cannot create asset at runtime. This should only be done in the editor.");
+      #endif
+    }
+  }
+
+  private void UnloadTerrain(Vector2Int tilePos) {
+    if (loadedTerrains.TryGetValue(tilePos, out Terrain terrain))
+    {
+      if (Application.isPlaying) {
+        Destroy(terrain.gameObject);
+      } else {
+        DestroyImmediate(terrain.gameObject);
+      }
+      loadedTerrains.Remove(tilePos);
+    }
+  }
+
+  private void LoadClickedSquares() {
+    bool[] squares = WorldStreamingEditor.GetClickedSquares();
+
+    if (squares == null) {
+      return;
+    }
+    //Debug.Log("Loading clicked squares...");
+    for (int i = 0; i < squares.Length; i++) {
+      if (squares[i]) {
+        if (loadedTerrains.ContainsKey(new Vector2Int(i % gridWidth, i / gridWidth))) {
+          continue;
+        }
+        // Load the square at position i
+        LoadTerrain(new Vector2Int(i % gridWidth, i / gridWidth));
+      } else {
+        if (!loadedTerrains.ContainsKey(new Vector2Int(i % gridWidth, i / gridWidth))) {
+          continue;
+        }
+        // Unload the square at position i
+        UnloadTerrain(new Vector2Int(i % gridWidth, i / gridWidth));
+      }
+    }
+  }
+
+}
---
+++ astoria-game/Assets/Environment/Scripts/WorldStreamingEditor.cs
@@ -0,0 +1,75 @@
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class WorldStreamingEditor : EditorWindow {
+  public delegate void GridSquareClicked(int x, int y);  // Delegate for callback
+
+  private int gridWidth = 2;
+  private int gridHeight = 2;
+  private float squareSize = 40f;  // Size of each square in the grid
+  private Color defaultColor = Color.red;
+  private Color clickColor = Color.green;
+
+  public static bool[] clickedSquares = null;  // Array to store which squares have been clicked
+
+  private GridSquareClicked onSquareClicked;
+
+  private WorldStreamingEditor() {
+    // Set the default callback function
+    onSquareClicked = (x, y) => ClickCallback(x, y);
+  }
+
+  private void ClickCallback(int x, int y) {
+    clickedSquares[y * gridWidth + x] = !clickedSquares[y * gridWidth + x];
+    Debug.Log("Clicked on square: " + x + ", " + y);
+  }
+
+  // Method to initialize the editor window
+  [MenuItem("Window/World Streaming Editor")]
+  public static void ShowWindow() {
+    GetWindow<WorldStreamingEditor>("World Streaming Editor");
+  }
+
+  private void OnGUI() {
+    // Initialize the clickedSquares array if it hasn't been created yet
+    if (clickedSquares == null) {
+      clickedSquares = new bool[gridWidth * gridHeight];
+    }
+
+    DrawGrid();
+  }
+
+  // Draws a grid of clickable squares
+  private void DrawGrid() {
+    // Title label
+    GUILayout.Label("World Streaming Grid", EditorStyles.boldLabel);
+
+    Rect gridRect = new Rect(10, 20, (gridWidth + 0.5f) * squareSize, (gridHeight + 0.5f) * squareSize);
+    GUILayout.BeginArea(gridRect);
+
+    for (int y = 0; y < gridHeight; y++) {
+      GUILayout.BeginHorizontal();
+      for (int x = 0; x < gridWidth; x++) {
+        // Set color based on whether the square has been clicked
+        GUI.backgroundColor = clickedSquares[y * gridWidth + x] ? clickColor : defaultColor;
+        if (GUILayout.Button(x + ", " + y, GUILayout.Width(squareSize), GUILayout.Height(squareSize))) {
+          // Trigger callback when a square is clicked
+          onSquareClicked?.Invoke(x, y);
+        }
+      }
+      GUILayout.EndHorizontal();
+    }
+
+    GUILayout.EndArea();
+  }
+
+  public static bool[] GetClickedSquares() {
+    return clickedSquares;
+  }
+
+  // Method to set the callback function
+  public void SetOnSquareClickedCallback(GridSquareClicked callback) {
+    onSquareClicked = callback;
+  }
+}
