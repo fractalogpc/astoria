5bd7cee80a3150eface26ae883549c1064ef242d
DoomKitty87 <doomkitty887@gmail.com>
2024-12-16 15:47:29 -0800

    reflection probe staging works i think

--- astoria-game/Assets/ReflectionProbeFollow.cs
+++ astoria-game/Assets/ReflectionProbeFollow.cs
@@ -1,13 +1,24 @@
 using Mirror;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Rendering.HighDefinition;
 
 public class ReflectionProbeFollow : MonoBehaviour
 {
 
   [SerializeField] private Transform _reflectionProbe;
+  [SerializeField] private Transform _secondaryReflectionProbe;
+  [SerializeField] private ReflectionProbe _reflectionProbeComponent;
+  [SerializeField] private ReflectionProbe _secondaryReflectionProbeComponent;
+  [SerializeField] private HDAdditionalReflectionData _reflectionProbeData;
+  [SerializeField] private HDAdditionalReflectionData _secondaryReflectionProbeData;
+
+  [SerializeField] private float _updateInterval = 0.5f;
   
   private Transform _localPlayer;
+
+  private float _timeSinceLastUpdate = 0;
+  private bool _toUpdatePrimary = true;
 
   private IEnumerator Start() {
     while (true) {
@@ -24,9 +35,43 @@ public class ReflectionProbeFollow : MonoBehaviour
     }
   }
 
-  void Update() {
+  private void Update() {
     if (_localPlayer == null) return;
-    _reflectionProbe.position = _localPlayer.position;
+
+    if (_reflectionProbeComponent.IsFinishedRendering(0) && !_toUpdatePrimary) {
+      // Blend the reflection probe towards weight 1
+
+      _reflectionProbeData.weight = _timeSinceLastUpdate / _updateInterval;
+
+      _timeSinceLastUpdate += Time.deltaTime;
+    } else if (_secondaryReflectionProbeComponent.IsFinishedRendering(0) && _toUpdatePrimary) {
+      // Blend the reflection probe towards weight 1
+
+      _secondaryReflectionProbeData.weight = _timeSinceLastUpdate / _updateInterval;
+
+      _timeSinceLastUpdate += Time.deltaTime;
+    }
+    
+    if (_timeSinceLastUpdate >= _updateInterval) {
+      _timeSinceLastUpdate = 0;
+
+      if (_toUpdatePrimary) {
+        _reflectionProbe.position = _localPlayer.position;
+
+        _reflectionProbeComponent.RenderProbe();
+
+        _reflectionProbeData.weight = 0;
+      } else {
+        _secondaryReflectionProbe.position = _localPlayer.position;
+
+        _secondaryReflectionProbeComponent.RenderProbe();
+
+        _secondaryReflectionProbeData.weight = 0;
+      }
+
+      _toUpdatePrimary = !_toUpdatePrimary;
+    }
+
   }
 
 }
