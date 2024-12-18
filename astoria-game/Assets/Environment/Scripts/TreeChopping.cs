d8c59ab4a81788dce8b525d9b5293c4dede2ae28
DoomKitty87 <doomkitty887@gmail.com>
2024-12-18 00:45:36 -0800

    made tree chopping remove collider
    bad for 1% lows, either need to split tiles or something else

--- astoria-game/Assets/Environment/Scripts/TreeChopping.cs
+++ astoria-game/Assets/Environment/Scripts/TreeChopping.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeChopping : MonoBehaviour
 {
 
   [SerializeField] private TerrainData _terrainData;
+  [SerializeField] private TerrainCollider _terrainCollider;
   [SerializeField] private float _treeSearchRadius = 1f;
   [SerializeField] private GameObject _treePrefab;
 
   private TreeInstance[] _treeInstances;
+
+  private List<TreeInstance> _removedTreeInstances = new List<TreeInstance>();
 
   public static TreeChopping Instance { get; private set; }
 
@@ -30,15 +34,34 @@ public class TreeChopping : MonoBehaviour
       if (Vector2.Distance(treePosition2D, position2D) < _treeSearchRadius) {
         Debug.Log("Found tree at " + treePosition);
         // Remove the tree instance
-        TreeInstance _newInstance = new TreeInstance();
-        _newInstance.prototypeIndex = _treeInstances[i].prototypeIndex;
-        _newInstance.position = _treeInstances[i].position;
-        _newInstance.widthScale = _treeInstances[i].widthScale;
-        _newInstance.heightScale = 0;
-        _newInstance.color = _treeInstances[i].color;
-        _newInstance.lightmapColor = _treeInstances[i].lightmapColor;
-        _newInstance.rotation = _treeInstances[i].rotation;
-        _terrainData.SetTreeInstance(i, _newInstance);
+        // TreeInstance _newInstance = new TreeInstance();
+        // _newInstance.prototypeIndex = _treeInstances[i].prototypeIndex;
+        // _newInstance.position = _treeInstances[i].position;
+        // _newInstance.widthScale = _treeInstances[i].widthScale;
+        // _newInstance.heightScale = 0;
+        // _newInstance.color = _treeInstances[i].color;
+        // _newInstance.lightmapColor = _treeInstances[i].lightmapColor;
+        // _newInstance.rotation = _treeInstances[i].rotation;
+        // _terrainData.SetTreeInstance(i, _newInstance);
+
+        // Create new list of tree instances without the chopped tree
+        TreeInstance[] newTreeInstances = new TreeInstance[_treeInstances.Length - 1];
+        int j = 0;
+        for (int k = 0; k < _treeInstances.Length; k++) {
+          if (k != i) {
+            newTreeInstances[j] = _treeInstances[k];
+            j++;
+          }
+        }
+
+        _removedTreeInstances.Add(_treeInstances[i]);
+
+        // Set the new tree instances
+        _treeInstances = newTreeInstances;
+        _terrainData.SetTreeInstances(newTreeInstances, false);
+
+        _terrainCollider.enabled = false;
+        _terrainCollider.enabled = true;
 
         // Instantiate a tree prefab at the tree position
         Instantiate(_treePrefab, treePosition, Quaternion.identity);
@@ -51,22 +74,31 @@ public class TreeChopping : MonoBehaviour
     Debug.Log("Regrowing tree at " + position);
 
     // Find the tree instance at the given position
-    for (int i = 0; i < _treeInstances.Length; i++) {
-      TreeInstance treeInstance = _treeInstances[i];
+    for (int i = 0; i < _removedTreeInstances.Count; i++) {
+      TreeInstance treeInstance = _removedTreeInstances[i];
       Vector3 treePosition = Vector3.Scale(treeInstance.position, _terrainData.size) + transform.position;
 
       if (Vector3.Distance(treePosition, position) < _treeSearchRadius) {
         Debug.Log("Found tree at " + treePosition);
         // Regrow the tree instance
-        TreeInstance _newInstance = new TreeInstance();
-        _newInstance.prototypeIndex = _treeInstances[i].prototypeIndex;
-        _newInstance.position = _treeInstances[i].position;
-        _newInstance.widthScale = _treeInstances[i].widthScale;
-        _newInstance.heightScale = _treeInstances[i].heightScale;
-        _newInstance.color = _treeInstances[i].color;
-        _newInstance.lightmapColor = _treeInstances[i].lightmapColor;
-        _newInstance.rotation = _treeInstances[i].rotation;
-        _terrainData.SetTreeInstance(i, _newInstance);
+        TreeInstance _newInstance = _removedTreeInstances[i];
+
+        // Create new list of tree instances with the regrown tree
+        TreeInstance[] newTreeInstances = new TreeInstance[_treeInstances.Length + 1];
+        for (int j = 0; j < _treeInstances.Length; j++) {
+          newTreeInstances[j] = _treeInstances[j];
+        }
+        newTreeInstances[_treeInstances.Length] = _newInstance;
+
+        _removedTreeInstances.RemoveAt(i);
+
+        // Set the new tree instances
+        _treeInstances = newTreeInstances;
+        _terrainData.SetTreeInstances(newTreeInstances, false);
+
+        _terrainCollider.enabled = false;
+        _terrainCollider.enabled = true;
+
         return;
       }
     }
