89627579cf14d4f2663aba593bf8ad8196ab0578
Ansel Stanik <doomkitty887@gmail.com>
2025-01-12 19:09:15 -0800

    set up moon and exposure and light cycle

--- astoria-game/Assets/DayNightCycle/TimeCycleSunLight.cs
+++ astoria-game/Assets/DayNightCycle/TimeCycleSunLight.cs
@@ -1,24 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 /*
 	Documentation: https://mirror-networking.gitbook.io/docs/guides/networkbehaviour
 	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
 */
 
-[RequireComponent(typeof(Light))]
 public class TimeCycleSunLight : MonoBehaviour
 {
- 	[SerializeField] private Light _directionalLight;
+ 	[SerializeField] private Light _sunLight;
+	[SerializeField] private Light _moonLight;
+	[SerializeField] private HDAdditionalLightData _moonLightData;
+
+	[SerializeField] private float _fullMoonIntensity = 1f;
+	[SerializeField] private float _newMoonIntensity = 0.5f;
+
 	private TimeCycleCore _timeCycleCore;
 	
 	private void Start() {
 		_timeCycleCore = TimeCycleCore.Instance;
-		_directionalLight = GetComponent<Light>();
 	}
 
 	private void Update() {
 		if (_timeCycleCore.TimeOfDay.DayLength == 0) return;
-		_directionalLight.transform.rotation = Quaternion.Euler(_timeCycleCore.TimeOfDay.SecsElapsed / _timeCycleCore.TimeOfDay.DayLength * 360 - 90, -90f, 0);
+		_sunLight.transform.rotation = Quaternion.Euler(_timeCycleCore.TimeOfDay.SecsElapsed / _timeCycleCore.TimeOfDay.DayLength * 360 - 90, -90f, 0);
+		_moonLight.transform.rotation = Quaternion.Euler(_timeCycleCore.TimeOfDay.SecsElapsed / _timeCycleCore.TimeOfDay.DayLength * 360 + 90, -90f, 0);
+
+		// Calculate moon phase
+		float moonPhase = _timeCycleCore.TimeOfDay.SecsElapsed / _timeCycleCore.TimeOfDay.DayLength % 29.5f / 29.5f;
+		_moonLightData.moonPhase = moonPhase;
+		_moonLight.intensity = Mathf.Lerp(_newMoonIntensity, _fullMoonIntensity, (moonPhase > 0.5f ? 1 - moonPhase : moonPhase) * 2);
 	}
 }
