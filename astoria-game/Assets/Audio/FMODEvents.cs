b040d76a9dfa9518d1ac0d9d3b1f7f123617d3a5
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-01-20 12:02:53 -0800

    added basic footstep audio

--- astoria-game/Assets/Audio/FMODEvents.cs
+++ astoria-game/Assets/Audio/FMODEvents.cs
@@ -4,5 +4,5 @@ using FMODUnity;
 public class FMODEvents : Singleton<FMODEvents>
 {
 	[Header("Player")]
-	[SerializeField] private EventReference _footstepsEvent;    
+	[field: SerializeField] public EventReference FootstepsEvent { get; private set; }
 }
