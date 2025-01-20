d578ae59ba7a002dc3c93d2b3485ad952f2b42f0
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-01-20 13:38:42 -0800

    removed debug spam

--- astoria-game/Assets/_Player/Scripts/FootstepAudio.cs
+++ astoria-game/Assets/_Player/Scripts/FootstepAudio.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FootstepAudio : MonoBehaviour
 {
     [SerializeField] private PlayerController _playerController;
-    [SerializeField] private float _footstepDistance = 1f;
+    [SerializeField] private float _footstepDistance = 1.8f;
     
     private EventReference _footstepsEvent;
     private Vector3 _lastFootstepPosition;
@@ -21,7 +21,6 @@ public class FootstepAudio : MonoBehaviour
         if (_playerController.MoveInputVector.magnitude < 0.1f) return;
         Vector3 positionXZ = new Vector3(transform.position.x, 0, transform.position.z);
         Vector3 lastPositionXZ = new Vector3(_lastFootstepPosition.x, 0, _lastFootstepPosition.z);
-        print(Vector3.Distance(positionXZ, lastPositionXZ));
         if (!(Vector3.Distance(positionXZ, lastPositionXZ) > _footstepDistance)) return;
         AudioManager.Instance.PlayOneShot(_footstepsEvent, transform.position);
         _lastFootstepPosition = transform.position;
