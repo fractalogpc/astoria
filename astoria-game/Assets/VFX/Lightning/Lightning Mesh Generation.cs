33b04bcea5c0c470f1bfec64755c458141a63684
NotSoGreeeen <jandrico7658@student.pps.net>
2025-02-10 13:50:39 -0800

    getting (mentally) ready to move to recursive lightning

--- astoria-game/Assets/VFX/Lightning/Lightning Mesh Generation.cs
+++ astoria-game/Assets/VFX/Lightning/Lightning Mesh Generation.cs
@@ -31,61 +31,12 @@ public class LightningMeshGeneration : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         
-        /*Bitangent[0] = goal.position - spawn.position;
-        Binormal[0] = new Vector3(Bitangent[0].x, Bitangent[0].z, -Bitangent[0].y) * Mathf.Sign(Bitangent[0].z);
-        Biongo[0] = (Vector3.Normalize(Bitangent[0]) + Vector3.Normalize(Binormal[0])) * Vector3.Dot(Vector3.Normalize(Bitangent[0]), Vector3.down);
-        Debug.DrawRay(spawn.position, Vector3.down, Color.black);
-        Debug.DrawRay(spawn.position, Bitangent[0], Color.red);
-        Debug.DrawRay(spawn.position, Binormal[0], Color.blue);
-        Debug.DrawRay(spawn.position, Biongo[0], Color.green);
-        for (int i = 1; i < 2; i++)
-        {
-            Bitangent[i] = goal.position - Biongo[i - 1];
-            Binormal[i] = new Vector3(Bitangent[i].x, Bitangent[i].z, -Bitangent[i].y) * Mathf.Sign(Bitangent[i].z);
-            Biongo[i] = (Vector3.Normalize(Bitangent[i]) + Vector3.Normalize(Binormal[i])) * Vector3.Dot(Vector3.Normalize(Bitangent[i]), Vector3.down);
-            Debug.DrawRay(Biongo[i - 1], Vector3.down, Color.black);
-            Debug.DrawRay(Biongo[i - 1], Bitangent[i], Color.red);
-            Debug.DrawRay(Biongo[i - 1], Binormal[i], Color.blue);
-            Debug.DrawRay(Biongo[i - 1], Biongo[i], Color.green);
-        }*/
-        
-        
-        /*for (int i = 0; i < numSegments; i++)
-        {
-            Random.seed = (int)Mathf.Round(Biongo[i].x + Biongo[i].y + Biongo[i].z) + 1000;
-            if (i == 0)
-            {
-                Bitangent[i] = goal.position - spawn.position;
-                Binormal[i] = new Vector3(Bitangent[i].x, Bitangent[i].z, -Bitangent[i].y) * Mathf.Sign(Bitangent[i].z);
-                Biongo[i + 1] = (spawn.position + (Vector3.Normalize(Bitangent[i]) + Vector3.Normalize(Binormal[i])) + Random.onUnitSphere * .2f);
-                //Debug.DrawRay(spawn.position, Vector3.down, Color.black);
-                //Debug.DrawRay(spawn.position, Bitangent[i], Color.red);
-                //Debug.DrawRay(spawn.position, Binormal[i], Color.blue);
-                Debug.DrawRay(spawn.position, Biongo[i + 1] - spawn.position, Color.green);
-            }
-            else
-            {
-                Bitangent[i] = goal.position - Biongo[i];
-                Binormal[i] = new Vector3(Bitangent[i].x, Bitangent[i].z, -Bitangent[i].y) * Mathf.Sign(Bitangent[i].z);
-                Biongo[i + 1] = (Biongo[i] + (Vector3.Normalize(Bitangent[i]) + Vector3.Normalize(Binormal[i])) + Random.onUnitSphere * .2f);
-                //Debug.DrawRay(Biongo[i], Vector3.down, Color.black);
-                //Debug.DrawRay(Biongo[i], Bitangent[i], Color.red);
-                //Debug.DrawRay(Biongo[i], Binormal[i], Color.blue);
-                Debug.DrawRay(Biongo[i], Biongo[i + 1] - Biongo[i], Color.green);
-            }
-
-            if (i == numSegments)
-            {
-                Debug.DrawRay(Biongo[i], goal.position, Color.green);
-            }
-        }*/
-        
-            SubVertexDirection.Clear();
-            SubVertexStart.Clear();
-            SubVertexGoal.Clear();
-            SubVertex.Clear();
-            FormLightning();
-            canDebug = true;
+        SubVertexDirection.Clear();
+        SubVertexStart.Clear();
+        SubVertexGoal.Clear();
+        SubVertex.Clear();
+        FormLightning();
+        canDebug = true;
 
         if (canDebug)
         {
@@ -165,7 +116,9 @@ public class LightningMeshGeneration : MonoBehaviour {
                     Vector3 point = Vector3.Lerp(SubVertexStart[i], SubVertexGoal[i], (float)j / 8) + Vector3.Lerp(Random.onUnitSphere, Vector3.Normalize(SubVertexGoal[i] - SubVertexStart[i]), lerp) * Mathf.Clamp(Vector3.Distance(goal.position, SubVertexStart[i]) / 5f, 1f, 100f);
                     SubVertex[i][j] = point; // Random.insideUnitSphere + line + SubVertexStart[i];
                 }
+                
             }
         }
     }
+    
 }
