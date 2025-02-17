fe55bcc67d8bc1365e51188c705a0468aaa259b8
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-02-17 13:00:03 -0800

    ragdoll disables collider on wanderer

--- astoria-game/Assets/AI/RagdollToggle.cs
+++ astoria-game/Assets/AI/RagdollToggle.cs
@@ -5,9 +5,11 @@ public class RagdollToggle : MonoBehaviour
 {
 	[SerializeField] private Animator _animator;
 	[SerializeField] private Rigidbody _rootRigidbody;
+	[SerializeField] private Collider _rootCollider;
 	
 	public void Ragdoll(bool ragdoll) {
 		_animator.enabled = !ragdoll;
+		_rootCollider.enabled = !ragdoll;
 		foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>()) {
 			rb.isKinematic = !ragdoll;
 		}
