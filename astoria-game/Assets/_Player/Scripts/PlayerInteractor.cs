70b7592d120e9ce714a34178ea1aa7fc30e1908e
Ansel Stanik <doomkitty887@gmail.com>
2025-02-20 08:45:34 -0800

    ground harvestables work
    we could do like rocks to collect if we want to

--- astoria-game/Assets/_Player/Scripts/PlayerInteractor.cs
+++ astoria-game/Assets/_Player/Scripts/PlayerInteractor.cs
@@ -7,6 +7,8 @@ using Mirror;
 public class PlayerInteractor : InputHandlerBase, IStartExecution
 {
   [SerializeField] private float _interactDistance = 4f;
+  [SerializeField] private float _interactRadius = 0.2f;
+  [SerializeField] private LayerMask _harvestableMask;
 
   private Camera _camera;
 
@@ -19,11 +21,28 @@ public class PlayerInteractor : InputHandlerBase, IStartExecution
   }
 
   private void Interact() {
-    if (!Physics.SphereCast(_camera.transform.position, 0.2f, _camera.transform.forward, out RaycastHit hit, _interactDistance)) {
-      print("did not hit anything");
-      return;
+    if (Physics.SphereCast(_camera.transform.position, _interactRadius, _camera.transform.forward, out RaycastHit hit, _interactDistance)) {
+      Interactable interactable = hit.collider.GetComponentInChildren<Interactable>();
+      if (interactable != null) {
+        interactable.Interact();
+        return;
+      }
     }
-    Interactable interactable = hit.collider.GetComponentInChildren<Interactable>();
-    interactable?.Interact();
+
+    // Try again with harvestables
+    if (Physics.SphereCast(_camera.transform.position, _interactRadius, _camera.transform.forward, out hit, _interactDistance, _harvestableMask)) {
+      GroundHarvestable harvestable = hit.collider.transform.parent.gameObject.GetComponent<GroundHarvestable>();
+      if (harvestable != null) {
+        // TODO: Trigger collect animation
+        (ItemData, int)[] harvestedItems = harvestable.Harvest();
+        foreach ((ItemData item, int amount) in harvestedItems) {
+          for (int i = 0; i < amount; i++) {
+            PlayerInstance.Instance.GetComponentInChildren<InventoryComponent>().AddItemByData(item);
+          }
+        }
+        return;
+      }
+    }
   }
+
 }
