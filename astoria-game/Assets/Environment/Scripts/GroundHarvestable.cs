a727c358f7ea2c9763b7b402873695aff9be55c1
Ansel Stanik <doomkitty887@gmail.com>
2025-02-20 00:05:01 -0800

    set up spawning thing

---
+++ astoria-game/Assets/Environment/Scripts/GroundHarvestable.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GroundHarvestable : MonoBehaviour
+{
+    
+    [System.Serializable]
+    public struct HarvestDropData
+    {
+
+        public ItemData item;
+        public int minAmount;
+        public int maxAmount;
+
+    }
+
+    [SerializeField] private HarvestDropData[] harvestDrops;
+
+    public (ItemData, int)[] Harvest()
+    {
+        List<(ItemData, int)> items = new List<(ItemData, int)>();
+        foreach (HarvestDropData drop in harvestDrops)
+        {
+            int amount = Random.Range(drop.minAmount, drop.maxAmount + 1);
+            items.Add((drop.item, amount));
+        }
+        return items.ToArray();
+
+        Destroy(gameObject);
+    }
+
+}
