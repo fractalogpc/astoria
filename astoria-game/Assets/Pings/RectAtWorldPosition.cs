40d5b2942014e0aa57c5049778f2010b5283d4a9
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-04-06 01:15:05 -0700

    made ping a prefab

--- astoria-game/Assets/Pings/RectAtWorldPosition.cs
+++ astoria-game/Assets/Pings/RectAtWorldPosition.cs
@@ -6,6 +6,8 @@ public class RectAtWorldPosition : MonoBehaviour
     public Transform OriginTransform => _originTransform;
     [Tooltip("The transform that the rect will calculate distance relative to.")]
     [SerializeField] private Transform _originTransform;
+    [Tooltip("If true, the current camera will be used as the origin transform.")]
+    [SerializeField] private bool _useCurrentCamera;
     public Vector3 WorldPosition => _worldPosition;
     [Tooltip("The world position that the rect represents.")]
     [SerializeField] private Vector3 _worldPosition;
@@ -29,17 +31,32 @@ public class RectAtWorldPosition : MonoBehaviour
     }
     
     /// <summary>
-    /// Set the origin transform that the rect will calculate distance relative to.
+    /// Set the origin transform that the rect will calculate distance relative to. Will disable automatic setting to camera.
     /// </summary>
     /// <param name="originTransform">The Transform to set.</param>
     public void SetOriginTransform(Transform originTransform) {
+        _useCurrentCamera = false;
         _originTransform = originTransform;
     }
-    
+
+    private void Start() {
+        if (_originTransform == null && _useCurrentCamera) {
+            Camera camera = Camera.main;
+            if (camera != null) {
+                _originTransform = camera.transform;
+            }
+        }
+    }
+
     private void Update() {
         float distance = Vector3.Distance(OriginTransform.position, WorldPosition);
         SetRectSize(_rectTransform, distance);
         SetRectPosition(_rectTransform, WorldPosition);
+        if (!_useCurrentCamera) return;
+        if (_originTransform != null) return;
+        Camera camera = Camera.current;
+        if (camera == null) return;
+        _originTransform = camera.transform;
     }
 
     private void SetRectSize(RectTransform rect, float distance) {
--- astoria-game/Assets/Pings/RectAtWorldPositionDistance.cs
+++ astoria-game/Assets/Pings/RectAtWorldPositionDistance.cs
@@ -9,6 +9,6 @@ public class RectAtWorldPositionDistance : MonoBehaviour
 
 	private void Update() {
 		float distance = Vector3.Distance(_rectAtWorldPosition.OriginTransform.position, _rectAtWorldPosition.WorldPosition);
-		_distanceText.text = $"{distance:F2}m";
+		_distanceText.text = $"{distance:N0}m";
 	}
 }
