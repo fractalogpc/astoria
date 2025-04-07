5097a4b5d6b1db54c6a92b343d68b5f8b5dba5d8
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-04-07 16:16:08 -0700

    added dialogue to cutscene, use timeline to add more
    you can add clip using add by dialogue data and sync it

---
+++ astoria-game/Assets/Dialogue/Scripts/Core/DialogueClip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+
+public class DialogueClip : PlayableAsset
+{
+	public DialogueData DialogueData;
+	
+	public DialogueClip() { }
+
+	public DialogueClip(DialogueData dialogueData) {
+		DialogueData = dialogueData;
+	}
+	
+	public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
+		DialogueBehavior behaviour = new();
+		behaviour.DialogueData = DialogueData;
+		return ScriptPlayable<DialogueBehavior>.Create(graph, behaviour);
+	}
+}
--- astoria-game/Assets/Dialogue/Scripts/Core/DialogueReceiver.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface DialogueReceiver
-{
-    public delegate void DialogueRecieved(Dialogue dialogue);
-    public event DialogueRecieved OnDialogueRecieved;
-    public void RecieveDialogue(Dialogue dialogue);
-}
