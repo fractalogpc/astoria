46809d627b8ccef88cb52010b4efe741bd07f373
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-04-11 00:15:16 -0700

    made map better, added player marker
    need to remove text from player marker

---
+++ astoria-game/Assets/MapMenu/Scripts/MapMarkerSystem/PlayerMarker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMarker : MonoBehaviour
+{
+	[SerializeField] private Transform _playerTransform;
+	[SerializeField] private MapMarkerManager _markerManager;
+	[SerializeField] private Sprite _markerIcon;
+	private MapMarker _marker;
+	
+	private void Start() {
+		_marker = new MapMarker("Player", _playerTransform.position, _markerIcon, _playerTransform.forward, false);
+		_markerManager.AddToMap(_marker);
+	}
+	
+	private void Update() {
+		if (_marker == null) return;
+		_marker.SetWorldPosition(_playerTransform.position);
+		_marker.SetDirectionFacing(_playerTransform.forward);
+	}
+}
