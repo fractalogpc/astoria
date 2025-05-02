0271d13bdee1fade47a4f495089f1e504bc61e54
Harper Wicker-Lenseigne <62197004+harperawl@users.noreply.github.com>
2025-05-02 09:47:32 -0700

    better

--- astoria-game/Assets/UpdateNavmeshBuildingNew.cs
+++ astoria-game/Assets/UpdateNavmeshBuildingNew.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.AI;
+using Unity.AI.Navigation;
+using System.Collections;
+using System.Collections.Generic;
 
 public class UpdateNavmeshBuildingNew : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    NavMeshSurface surface;
+    int childCount;
+    void Start() {
+        surface = GetComponent<NavMeshSurface>();
+        childCount = transform.childCount;
+    }
+    void Update() {
+        if (childCount != transform.childCount) {
+            // var sources = new List<NavMeshBuildSource>();
+            // var markups = new List<NavMeshBuildMarkup>();
+
+            // NavMeshBuilder.CollectSources(transform, surface.layerMask, NavMeshCollectGeometry.PhysicsColliders, surface.defaultArea, markups, sources);
+
+            // // Define bounds based on collected sources
+            // var bounds = NavMeshBuilder.CalculateWorldBounds(sources);
+
+            // NavMeshBuilder.UpdateNavMeshDataAsync(surface.navMeshData, surface.GetBuildSettings(), sources, bounds);
+        }
+        foreach (Transform child in transform) {
+            if (child.gameObject.tag == "Foundation" && !child.gameObject.TryGetComponent<NavMeshLink>(out NavMeshLink link)) {
+                AddNavMeshLinks(child.gameObject);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void AddNavMeshLinks(GameObject foundation) {
+        for (int i = 0; i < 4; i++) {
+            NavMeshLink tempLink = foundation.AddComponent<NavMeshLink>();
+            tempLink.startPoint = new Vector3(Mathf.Cos(Mathf.PI/2 * i) * 1.48f, 0, Mathf.Sin(Mathf.PI/2 * i) * 1.48f);
+            tempLink.endPoint = new Vector3(Mathf.Cos(Mathf.PI/2 * i) * 1.53f, 0, Mathf.Sin(Mathf.PI/2 * i) * 1.53f);
+            tempLink.width = 3;
+        }
     }
 }
