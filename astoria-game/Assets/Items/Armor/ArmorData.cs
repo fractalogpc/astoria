d4821f3bb4360c560d3c0e204401dc3cd6e982b7
Ansel Stanik <doomkitty887@gmail.com>
2025-05-09 22:11:28 -0700

    made armor obtainable + in game

---
+++ astoria-game/Assets/Items/Armor/ArmorData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Armor", menuName = "Scriptable Objects/Items/ArmorData")]
+public class ArmorData : ItemData
+{
+	
+    public float _armorValue;
+
+}
